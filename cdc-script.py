d6e4a1ae250da0c2616245803bd4bda3a3584a1d
Edmond Soun <edmond.soun@gmail.com>
2022-11-30 10:48:16 -0800

    adds env setup, notes for new timestamp bounds

--- cdc-script.py
+++ cdc-script.py
@@ -7,33 +7,35 @@ Needs to send a query that limits the returned information to that 24 hour perio
 Write that 24 period's worth of information to appropriately coded file
 *A DIFFERENT script will then parse that information and update our DB
 
+Idea for improvement: ping API for latest update and use this as lower bound timestamp?
+Because otherwise this may miss data if updates are not consistent day to day
+
 """
 
+# ENV SETUP
+import os
+from dotenv import load_dotenv
+
+# SOCRATA DEPENDENCIES
 import pandas as pd
-from datetime import datetime, timedelta
 from sodapy import Socrata
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-client = Socrata("data.oaklandca.gov", None)
+load_dotenv()
 
-START_DATE = datetime.now() - timedelta(days=1)
-# Example authenticated client (needed for non-public datasets):
-# client = Socrata(data.oaklandca.gov,
-#                  MyAppToken,
-#                  username="user@example.com",
-#                  password="AFakePassword")
+API_TOKEN = os.environ['TOKEN']
+DATA_URL = "data.oaklandca.gov"
+DATA_SET = "ppgh-7dqv"
+
+client = Socrata(DATA_URL, API_TOKEN)
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("ppgh-7dqv",
+results = client.get(DATA_SET,
                     where="""
                         date_diff_d(to_floating_timestamp(get_utc_date(), 'UTC'), `datetime`) <= 1 
-                        AND date_diff_d(to_floating_timestamp(get_utc_date(), 'UTC'), `datetime`) >= 0""",
+                        AND date_diff_d(to_floating_timestamp(get_utc_date(), 'UTC'), `datetime`) > 0""",
                     order="datetime ASC"
                     )
 
-# Convert to pandas DataFrame
 print(results)
-
-print(START_DATE)
+# print(START_DATE)
